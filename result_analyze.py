17e4b04c53fa92b44a3d685a9afd5301dcba299a
xiz675 <xiz675@eng.ucsd.edu>
2020-03-01 02:31:45 -0800

    remove empty twees, tags

--- result_analyze.py
+++ result_analyze.py
@@ -1,24 +1,46 @@
 import pickle
-from sklearn.model_selection import train_test_split
-from create_input import write_to_file
+import numpy as np
+#from sklearn.model_selection import train_test_split
+
+
+
+def write_to_file(file_path, entities):
+    f = open(file_path, "w", encoding="utf-8")
+    for t in entities:
+        f.write(t)
+        f.write("\n")
+    f.close()
 
 
 def creat_input_from_matching(df, data_path):
     all_tweets = []
     all_news = []
     all_tags = []
-    for index, tweet in df.iterrows():
-        all_tweets.append(tweet["processed_tweet"])
-        all_tags.append(tweet["hashtags"])
-        clean_news_total = ' '.join(tweet["news"].split(";"))
+    for ind in df.index:
+        if len(df['hashtags'][ind]) == 0 or len(df['processed_tweet'][ind]) == 0 or len(df['news'][ind]) == 0:
+            continue
+        all_tweets.append(df["processed_tweet"][ind])
+        all_tags.append(df['hashtags'][ind])
+        clean_news_total = ' '.join(df['news'][ind].split(';'))
         all_news.append(clean_news_total)
+    index = np.arange(len(all_tweets))
+    np.random.shuffle(index)
+    all_tweets = np.array(all_tweets)
+    all_tags = np.array(all_tags)
+    all_news = np.array(all_news)
+    train_index = index[:int(len(all_tweets)*0.8)]
+    valid_index = index[int(len(all_tweets)*0.8):int(len(all_tweets)*0.9)]
+    test_index = index[int(len(all_tweets)*0.9):]
+    tweets_train, tweets_val, tweets_test = all_tweets[train_index], all_tweets[valid_index], all_tweets[test_index]
+    news_train, news_val, news_test = all_news[train_index], all_news[valid_index], all_news[test_index]
+    tags_train, tags_val, tags_test = all_tags[train_index], all_tags[valid_index], all_tags[test_index]
 
-    tweets_train, tweets_test, news_train, news_test, tags_train, tags_test = train_test_split(all_tweets, all_news,
-                                                                                               all_tags, test_size=0.1,
-                                                                                               random_state=42)
-    tweets_train, tweets_val, news_train, news_val, tags_train, tags_val = train_test_split(tweets_train, news_train,
-                                                                                            tags_train, test_size=0.1,
-                                                                                            random_state=42)
+    # tweets_train, tweets_test, news_train, news_test, tags_train, tags_test = train_test_split(all_tweets, all_news,
+    #                                                                                            all_tags, test_size=0.1,
+    #                                                                                            random_state=42)
+    # tweets_train, tweets_val, news_train, news_val, tags_train, tags_val = train_test_split(tweets_train, news_train,
+    #                                                                                         tags_train, test_size=0.1,
+    #                                                                                         random_state=42)
 
     write_to_file(data_path + "train_post.txt", tweets_train)
     write_to_file(data_path + "train_conv.txt", news_train)
